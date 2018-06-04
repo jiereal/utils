65d1c362ead3b5075ffa4b8f4867d4a8b3d3cc77
xieyingjie <xieyingjie@sf-express.com>
2018-06-04 16:37:40 +0800

    add vuexHelper.js

---
+++ vuexHelper.js
@@ -0,0 +1,36 @@
+import lodash from 'lodash';
+
+function createMutations(initialState) {
+    return Object.keys(initialState).reduce((obj, key) => {
+        obj[`${key}.set`] = (state, value) => {
+            if (initialState[key] === null || lodash.isPlainObject(initialState[key])) {
+                state[key] = Object.assign({}, state[key], lodash.cloneDeep(value));
+            } else {
+                state[key] = value;
+            }
+        };
+        return obj;
+    }, {});
+}
+
+export default function (vuex) {
+    const {createNamespacedHelpers} = vuex;
+    return function createNamespacedStore(name, {state: initialState, mutations = {}, actions = {}, getters = {}}) {
+        const {mapMutations, mapState, mapActions, mapGetters} = createNamespacedHelpers(name);
+        const _mutations = {
+            ...createMutations(initialState),
+            ...mutations
+        };
+        return {
+            namespaced: true,
+            state: lodash.cloneDeep(initialState),
+            getters,
+            actions,
+            mutations: _mutations,
+            mapState,
+            mapMutations,
+            mapGetters,
+            mapActions
+        };
+    }
+};
